8dc0d282ce53d587908d2bc36af7c6a9184c46e9
Win Lwin Oo <winlwinoocu@gmail.com>
2021-09-06 17:41:26 +0630

    add product type service

--- src/product-type/product-type.controller.ts
+++ src/product-type/product-type.controller.ts
@@ -1,36 +1,76 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query, ParseIntPipe } from '@nestjs/common';
 import { ProductTypeService } from './product-type.service';
 import { CreateProductTypeDto } from './dto/create-product-type.dto';
 import { UpdateProductTypeDto } from './dto/update-product-type.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { ProductTypeDto } from './dto/product-type.dto';
 
 @ApiTags('product-types')
 @Controller('product-types')
+@UseGuards(AuthGuard())
 export class ProductTypeController {
-  constructor(private readonly productTypeService: ProductTypeService) {}
+  constructor(private readonly productTypeService: ProductTypeService) { }
 
+  /**
+   * create new product type
+   */
+  @ApiCreatedResponse({ type: ProductTypeDto, description: 'Response cerated product type' })
+  @ApiNotFoundResponse()
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
   @Post()
-  create(@Body() createProductTypeDto: CreateProductTypeDto) {
-    return this.productTypeService.create(createProductTypeDto);
+  async create(@Body() createProductTypeDto: CreateProductTypeDto): Promise<ProductTypeDto> {
+    return await this.productTypeService.create(createProductTypeDto);
   }
 
+  /**
+   * find all product type OR
+   * search product type by product_type_name
+   */
+  @ApiOkResponse({ type: ProductTypeDto, isArray: true, description: 'Response all product type or search product type by product_type_name' })
+  @ApiQuery({ name: 'product_type_name', required: false })
+  @ApiNotFoundResponse()
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
   @Get()
-  findAll() {
-    return this.productTypeService.findAll();
+  async findAll(@Query('product_type_name') product_type_name?: string): Promise<ProductTypeDto[]> {
+    return await this.productTypeService.findAll(product_type_name);
   }
 
+  /**
+   * get product type by id
+   */
+  @ApiOkResponse({ type: ProductTypeDto, description: 'Response search product type' })
+  @ApiNotFoundResponse()
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productTypeService.findOne(+id);
+  async findOne(@Param('id', ParseIntPipe) id: number): Promise<ProductTypeDto> {
+    return this.productTypeService.findOne(id);
   }
 
+  /**
+   * update product type by id
+   */
+  @ApiOkResponse({ type: ProductTypeDto, description: 'Response updated product type' })
+  @ApiNotFoundResponse()
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductTypeDto: UpdateProductTypeDto) {
-    return this.productTypeService.update(+id, updateProductTypeDto);
+  async update(@Param('id', ParseIntPipe) id: number, @Body() updateProductTypeDto: UpdateProductTypeDto): Promise<ProductTypeDto> {
+    return await this.productTypeService.update(id, updateProductTypeDto);
   }
 
+  /**
+   * delete product type by id
+   */
+  @ApiOkResponse({ type: ProductTypeDto, description: 'Response deleted product type' })
+  @ApiNotFoundResponse()
+  @ApiBadRequestResponse()
+  @ApiInternalServerErrorResponse()
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productTypeService.remove(+id);
+  async remove(@Param('id', ParseIntPipe) id: number): Promise<ProductTypeDto> {
+    return await this.productTypeService.remove(id);
   }
 }
--- src/product-type/product-type.service.ts
+++ src/product-type/product-type.service.ts
@@ -1,26 +1,112 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { toProductTypeDto } from 'src/helper/mapper/product-type.mapper';
+import { Repository } from 'typeorm';
 import { CreateProductTypeDto } from './dto/create-product-type.dto';
+import { ProductTypeDto } from './dto/product-type.dto';
 import { UpdateProductTypeDto } from './dto/update-product-type.dto';
+import { ProductType } from './entities/product-type.entity';
+const logger = new Logger('ProductTypeService');
 
 @Injectable()
 export class ProductTypeService {
-  create(createProductTypeDto: CreateProductTypeDto) {
-    return 'This action adds a new productType';
+  constructor(@InjectRepository(ProductType) private readonly productTypeRepository: Repository<ProductType>) { }
+  /**
+   * create new product type data
+   * return ProductTypeDto
+   */
+  async create(createProductTypeDto: CreateProductTypeDto): Promise<ProductTypeDto> {
+    const { protuct_type_name } = createProductTypeDto;
+
+    const productTypeInDb = await this.productTypeRepository.findOne({ where: { protuct_type_name } });
+    if (productTypeInDb) {
+      throw new BadRequestException({ message: 'Product type already exit' });
+    }
+    const productType: ProductType = this.productTypeRepository.create(createProductTypeDto);
+    try {
+      await this.productTypeRepository.save(productType);
+    } catch (error) {
+      logger.error(`create: ${error}`);
+      throw new InternalServerErrorException({ message: 'Create product type fail' });
+    }
+    return toProductTypeDto(productType);
   }
 
-  findAll() {
-    return `This action returns all productType`;
+  /**
+   * find product type data
+   * return ProductTypeDto[]
+   */
+  async findAll(protuct_type_name?: string): Promise<ProductTypeDto[]> {
+    try {
+      if (protuct_type_name) {
+        const product_type = await this.productTypeRepository.find({ where: { protuct_type_name } });
+        return product_type.map(category => toProductTypeDto(category));
+      } else {
+        const product_type = await this.productTypeRepository.find();
+        return product_type.map(category => toProductTypeDto(category));
+      }
+    } catch (error) {
+      logger.error(`findAll: ${error}`);
+      throw new BadRequestException({ message: 'Product type not found' });
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} productType`;
+  /**
+   * find product type data by id
+   * return ProductType Entity
+   */
+  async findOne(id: number): Promise<ProductType> {
+    try {
+      return await this.productTypeRepository.findOneOrFail(id);
+    } catch (error) {
+      logger.warn(`findOne : ${error}`);
+      throw new BadRequestException({ message: 'Product type not found' });
+    }
   }
 
-  update(id: number, updateProductTypeDto: UpdateProductTypeDto) {
-    return `This action updates a #${id} productType`;
+  /**
+   * find product type data by id
+   * return ProductTypeDto
+   */
+  async findById(id: number): Promise<ProductTypeDto> {
+    try {
+      const product_type = await this.findOne(id);
+      return toProductTypeDto(product_type)
+    } catch (error) {
+      throw new BadRequestException({ message: 'Product type not found' });
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} productType`;
+  /**
+   * Update product type row data that matches given id
+   * return ProductTypeDto
+   */
+  async update(id: number, updateProductTypeDto: UpdateProductTypeDto): Promise<ProductTypeDto> {
+    const product_type = await this.findOne(id);
+    if (!product_type) {
+      throw new BadRequestException({ message: 'Product type not found' });
+    }
+    try {
+      const productTypeToUpdate = Object.assign(product_type, updateProductTypeDto);
+      await this.productTypeRepository.update(id, productTypeToUpdate);
+    } catch (error) {
+      logger.error(`update : ${error}`);
+      throw new InternalServerErrorException({ message: 'Update product type fail' })
+    }
+    const updateProductType = await this.findOne(product_type.id);
+    return toProductTypeDto(updateProductType);
+  }
+
+  /**
+   * Delete entre product type row data that matches given id.
+   * return ProductTypeDto
+   */
+  async remove(id: number): Promise<ProductTypeDto> {
+    const product_type = await this.findOne(id);
+    if (!product_type) {
+      throw new BadRequestException({ message: 'Product type not found' });
+    }
+    const deleteProductType = await this.productTypeRepository.remove(product_type);
+    return toProductTypeDto(deleteProductType);
   }
 }
