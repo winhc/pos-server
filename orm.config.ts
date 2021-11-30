1481851f01af378ecfbe2d80a35697c46b5d3afe
Win Lwin Oo <winlwinoocu@gmail.com>
2021-11-30 13:12:54 +0630

    db with postgres

--- orm.config.ts
+++ orm.config.ts
@@ -1,14 +1,30 @@
-import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
-
-const ormConfig: MysqlConnectionOptions = {
-    type: "mysql",
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+// postgres
+const ormConfig: PostgresConnectionOptions = {
+    type: "postgres",
     host: "localhost",
-    port: 3306,
-    username: "developer",
-    password: "developer",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
     database: "cherry_central_db",
     entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
     synchronize: true
 }
 
 export default ormConfig;
+
+// mysql
+// import { MysqlConnectionOptions } from "typeorm/driver/mysql/MysqlConnectionOptions";
+
+// const ormConfig: MysqlConnectionOptions = {
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "developer",
+//     password: "developer",
+//     database: "cherry_central_db",
+//     entities: [__dirname + '/src/**/*.entity{.ts,.js}'],
+//     synchronize: true
+// }
+
+// export default ormConfig;
